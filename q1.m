ed171195be94dc8c2c67ec7b47dd14b04f3037c6
DeanLym <yiminliu@stanford.edu>
2015-10-25 14:32:52 -0700

    Add files q1

--- q1.m
+++ q1.m
@@ -1,7 +1,19 @@
 function q1()
+    close all;
     m = 200;
-    K = ones(m,1);
-    
+    K = 1e-6*ones(m,1);
+    Dx = 1/m;
+    phi0L=1; phi0R=1;
+    N = 1E-5*ones(m,1);
+    [A,b] = makeab1(K,N,Dx,phi0L,phi0R);
+    phi = A\b;
+    x = linspace(0,1,m);
+    data = load('head_obs.txt');
+    x_obs = data(:,1); phi_obs = data(:,2);
+    figure;
+    plot(x,phi,'k.-');
+    hold on;
+    plot(x_obs,phi_obs,'ro');
 
 
 
